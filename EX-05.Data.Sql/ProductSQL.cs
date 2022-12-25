0a65ce4bfd3650ed96face259571d53ad9fb0032
Amiroffir <amirosoffir@gmail.com>
2022-12-25 17:57:43 +0200

    second commit

---
+++ EX-05.Data.Sql/ProductSQL.cs
@@ -0,0 +1,44 @@
+﻿using EX_05.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_05.Data.Sql
+{
+    // This class is responsible for all the SQL queries
+    public class ProductSQL
+    {
+        Dictionary<int, Product> productsList = new Dictionary<int, Product>();
+        public Dictionary<int, Product> getProductsFromDB()
+        {
+           
+            DAL.SQLQuery.runCommand("SELECT * FROM [dbo].[Products]", GetProducts);
+            return productsList;
+        }
+        private void GetProducts(SqlDataReader reader)
+        {
+            productsList.Clear();
+            while (reader.Read())
+            {
+
+                Product product = new Product();
+                product.ProductID = reader.GetInt32(0);
+                product.ProductName = reader.GetString(1);
+                product.SupplierID = reader.GetInt32(2);
+                product.CategoryID = reader.GetInt32(3);
+                product.QuantityPerUnit = reader.GetString(4);
+                product.UnitPrice = reader.GetDecimal(5);
+                product.UnitsInStock = reader.GetInt16(6);
+                product.UnitsOnOrder = reader.GetInt16(7);
+                product.ReorderLevel = reader.GetInt16(8);
+                product.Discontinued = reader.GetBoolean(9);
+
+               productsList.Add(product.ProductID, product);
+            }
+        }
+
+    }
+}
--- EX_05.Entities/ProductEntity.cs
+++ EX_05.Entities/ProductEntity.cs
@@ -13,36 +13,18 @@ namespace EX_05.Entities
     {
 
 
-         public void GetProductsFromDB()
-        {
-            MainManager.Instance.ProductsList.Clear();
-            DAL.SQLQuery.runCommand("SELECT * FROM [dbo].[Products]", GetProducts);
-        }
+         
         public Product GetProductByID(int id)
         {
             return MainManager.Instance.ProductsList[id];
         }
 
-
-        private void  GetProducts (SqlDataReader reader)
+        public void GetProductsFromDB()
         {
-            while (reader.Read())
-            {
+            Data.Sql.ProductSQL productSQL = new Data.Sql.ProductSQL();
+            MainManager.Instance.ProductsList = productSQL.getProductsFromDB();
 
-               Product product = new Product();
-               product.ProductID = reader.GetInt32(0);
-               product.ProductName = reader.GetString(1);
-               product.SupplierID = reader.GetInt32(2);
-               product.CategoryID = reader.GetInt32(3);
-               product.QuantityPerUnit = reader.GetString(4);
-               product.UnitPrice = reader.GetDecimal(5);
-               product.UnitsInStock = reader.GetInt16(6);
-               product.UnitsOnOrder = reader.GetInt16(7);
-               product.ReorderLevel = reader.GetInt16(8);
-               product.Discontinued = reader.GetBoolean(9);
-      
-               MainManager.Instance.ProductsList.Add(product.ProductID, product);
-            }
         }
+
     }
 }
